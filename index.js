56e57c28794941dacab598c1137c0e5365f7014d
anhnguyentrung <nguyentrunganh.bk@gmail.com>
2018-12-06 17:11:58 +0700

    send nep5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { default: Neon, api, wallet } = require("@cityofzion/neon-js")
+const { default: Neon, api, wallet, u } = require("@cityofzion/neon-js")
 const toAddress = 'ALq7AWrhAueN6mJNqk6FHJjnsEoPRytLdW'
 const network = "TestNet"
 //1. create account
@@ -13,12 +13,41 @@ const apiProvider = new api.neoscan.instance(network)
 console.log("\n\n--- API Provider ---")
 console.log(apiProvider)
 //4. send asset
-const config = {
-  api: apiProvider,
-  account: account,
-  intents: intent
+// const config = {
+//   api: apiProvider,
+//   account: account,
+//   intents: intent
+// }
+// Neon.sendAsset(config)
+//   .then(config => {
+//     console.log("\n\n--- Response ---")
+//     console.log(config.response)
+//   })
+//   .catch(config => {
+//     console.log(config)
+//   })
+
+//send nep5 token
+//script hash of SCC
+const sccTokenScriptHash = "323571cfc42a40d48d64832a7da594039fbac76a"
+const sccTransferAmount = 100.001
+//make intent
+const intent2 = api.makeIntent({ GAS: 0.00000001 }, account.address)
+const config2 = {
+	api: apiProvider,
+	account: account,
+	intents: intent2,
+	script: {
+    	scriptHash: sccTokenScriptHash,
+    	operation: 'transfer',
+    	args: [
+      		u.reverseHex(wallet.getScriptHashFromAddress(account.address)),
+      		u.reverseHex(wallet.getScriptHashFromAddress(toAddress)),
+      		new u.Fixed8(sccTransferAmount).toReverseHex(),
+    	],
+  	}
 }
-Neon.sendAsset(config)
+Neon.doInvoke(config2)
   .then(config => {
     console.log("\n\n--- Response ---")
     console.log(config.response)
@@ -26,4 +55,7 @@ Neon.sendAsset(config)
   .catch(config => {
     console.log(config)
   })
+
+
+
 
